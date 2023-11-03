357e82a2c41e3f0184dbf332e574161228a6a2f4
johansjacob <getjohan124@gmail.com>
2023-11-03 13:13:26 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,35 @@
+'''Start with the list you used in Exercise 1, but instead of just
+
+printing each person's name, print a message to them. The text of each message should be the same, but each message should be 
+
+personalized with the person's name.'''
+
+#list same as used in exercise 1
+a=['Namith','Sathwik','Jerom','Alan','Sheehan','Sabah','Afnan','Alon','Akhinesh']
+
+print('My name is:',a[0])
+print()
+
+print('My name is:',a[1])
+print()
+
+print('My name is:',a[2])
+print()
+
+print('My name is:',a[3])
+print()
+
+print('My name is:',a[4])
+print()
+
+print('My name is:',a[5])
+print()
+
+print('My name is:',a[6])
+print()
+
+print('My name is:',a[7])
+print()
+
+print('My name is:',a[8])
+print()
