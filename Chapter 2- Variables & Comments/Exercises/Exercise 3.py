b7ff41dceb29d1035d5c44a8667544b48e77c645
johansjacob <getjohan124@gmail.com>
2023-11-03 12:13:39 +0400

    Exercise 3.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 3.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 3.py
@@ -5,3 +5,54 @@ Use a variable to represent a person's name, and include some whitespace charact
 Print the name once, so the whitespace around the name is displayed. 
 
 Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().'''
+
+
+#using \t function
+
+x='   Johan\tJacob    '
+print(x)
+
+print()
+
+#performing lstrip()
+a=x.lstrip()
+print(a)
+
+print()
+
+#performing rstrip()
+b=x.rstrip()
+print(b)
+
+print()
+
+#performing strip()
+c=x.strip()
+print(c)
+
+print()
+print('__________________________________________________')
+
+#using \n function
+y='   Johan\n   Jacob    '
+print(y)
+
+print()
+
+#performing lstrip()
+d=y.lstrip()
+print(d)
+
+print()
+
+#performing rstrip()
+e=y.rstrip()
+print(e)
+
+print()
+
+#performing strip()
+f=y.strip()
+print(f)
+
+print()
