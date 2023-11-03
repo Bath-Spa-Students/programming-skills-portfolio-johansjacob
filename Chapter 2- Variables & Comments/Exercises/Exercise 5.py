816a3ae53bbec21e4f05dbef0a1c70ad8f70144c
johansjacob <getjohan124@gmail.com>
2023-11-03 12:36:10 +0400

    Exercise 5.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 5.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -3,3 +3,20 @@
 Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
 
 You will to use the arithmetic operators to complete this exercise.'''
+
+#Number of USB sticks the girl wants
+x=50
+
+#Cost of 1 USB stick
+y=6
+
+#Number of USB sticks she can buy
+z=x/y
+print('Number of USB stick she can buy: ',round(z))
+
+print()
+
+#Amount of pounds she has left
+a=x-y*round(z)
+print('The amount of pounds she has left after the purchase is:',a)
+
