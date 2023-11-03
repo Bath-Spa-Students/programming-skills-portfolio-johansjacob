7f3c3e06c52c73ef57df2d1ab60fb35b12cb6b8a
johansjacob <getjohan124@gmail.com>
2023-11-03 12:18:20 +0400

    Exercise 4.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 4.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise 4.py
@@ -1,2 +1,10 @@
 '''Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print
 that message.'''
+
+#assigning my favorite number to a variable
+x='4'
+print(x)
+
+#using the same variable to assign a message 
+x='This is my favorite number'
+print(x)
