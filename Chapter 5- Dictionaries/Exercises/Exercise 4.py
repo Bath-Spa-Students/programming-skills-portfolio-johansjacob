bfe19046fb2084f3ba73fe261bcda2dd5170d3cb
johansjacob <getjohan124@gmail.com>
2023-11-15 21:53:49 +0400

    Exercise 4.py

--- Chapter 5- Dictionaries/Exercises/Exercise 4.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 4.py
@@ -5,3 +5,18 @@
 * Use a loop to print the name of each river included in the dictionary.
 
 * Use a loop to print the name of each country included in the dictionary.'''
+
+rivers = {'nile': 'egypt',
+    'hudson': 'united states',
+    'thames': 'united kingdom'}
+
+for river, country in rivers.items():
+    print(f"The {river.title()} flows through {country.title()}.")
+
+print("\nThe following rivers are included in this dictionary:")
+for river in rivers.keys():
+    print(f"- {river.title()}")
+
+print("\nThe following countries are included in this dictonary:")
+for country in rivers.values():
+    print(f"- {country.title()}")
