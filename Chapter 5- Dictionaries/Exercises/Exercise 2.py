34406af83ceef09056f1f420fe49d7b6d3e79c29
johansjacob <getjohan124@gmail.com>
2023-11-15 19:38:20 +0400

    Exercise 2.py

--- Chapter 5- Dictionaries/Exercises/Exercise 2.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -9,3 +9,21 @@ their meanings as values.
 the word on one line and then print its meaning indented on a second line. Use the newline character (\n) to insert a blank line between 
 
 each word-meaning pair in your output.'''
+
+#creating a dictionary consisting of five programming words learnerd in previous chapters
+glossary = {'if': 'This condition can be any expression that evaluates to either True or False','elif': 'it is used to include multiple conditional expressions after the if condition or even in between if and else conditions. ','else': 'it is used in conditional if statements, and decides what to do if the condition is False.','print': 'it is used to display a ceratin messge or call out a variable','input':'it helps the user to input the values' }
+
+word = 'if'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'elif'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'else'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'print'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'input'
+print(f"\n{word.title()}: {glossary[word]}")
