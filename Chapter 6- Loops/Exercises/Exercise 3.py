ad4e1f1a0a4bb99d4bcf23279b5a0c4810ada5d4
johansjacob <getjohan124@gmail.com>
2023-11-15 22:21:42 +0400

    Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,6 +1,8 @@
 '''Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.)'''
 
+#assigning a variable
 x='Hi'
 
+#creating the infinite loop
 while x <='Hi':
     print(x)
