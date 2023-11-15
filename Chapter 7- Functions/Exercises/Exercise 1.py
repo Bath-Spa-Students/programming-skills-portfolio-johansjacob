fef1c08a7fdb48275d368fa4dba031e7d5482efc
johansjacob <getjohan124@gmail.com>
2023-11-15 22:38:15 +0400

    Exercise 1.py

---
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+'''Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the
+
+function, and make sure the message displays correctly.'''
+
+def display_message():
+
+#displaying about what I'm learning
+    message = "I'm learning to store code in functions."
+    print(message)
+
+display_message()
