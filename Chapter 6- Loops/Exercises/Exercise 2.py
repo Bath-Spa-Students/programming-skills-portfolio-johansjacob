0bc55f378f4a9fd42ceb5e1553f3ab704d9bd074
johansjacob <getjohan124@gmail.com>
2023-11-15 22:12:37 +0400

    Exercise 2.py

--- Chapter 6- Loops/Exercises/Exercise 2.py
+++ Chapter 6- Loops/Exercises/Exercise 2.py
@@ -3,3 +3,21 @@
 they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop in which you ask users their 
 
 age, and then tell them the cost of their movie ticket'''
+
+#creating a prompt for the user 
+x = "How old are you?"
+x += "\nEnter 'quit' when you are finished. "
+
+while True:
+    age = input(x)
+    if age == 'quit':
+        break
+    age = int(age)
+
+#displaying the message according to the condition
+    if age < 3:
+        print("  You get in free!")
+    elif age < 13:
+        print("  Your ticket is $10.")
+    else:
+        print("  Your ticket is $15.")
