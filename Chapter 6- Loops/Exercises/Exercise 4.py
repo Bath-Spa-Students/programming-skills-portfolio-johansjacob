d40d6d169e68a4f94335cd85dd096b114bdc216f
johansjacob <getjohan124@gmail.com>
2023-11-15 22:31:48 +0400

    Exercise 4.py

--- Chapter 6- Loops/Exercises/Exercise 4.py
+++ Chapter 6- Loops/Exercises/Exercise 4.py
@@ -10,9 +10,9 @@ finished_sandwiches = []
 
 while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
-    print(f"I'm working on your {current_sandwich} sandwich.")
+    print(f'I am working on your {current_sandwich} sandwich.')
     finished_sandwiches.append(current_sandwich)
 
-print("\n")
+print('\n')
 for sandwich in finished_sandwiches:
-    print(f"I made a {sandwich} sandwich.")
+    print(f'I made a {sandwich} sandwich.')
