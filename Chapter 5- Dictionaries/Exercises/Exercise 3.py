85f44c7caec45e587fa857566e9e1a9ef72ba2f8
johansjacob <getjohan124@gmail.com>
2023-11-15 19:57:24 +0400

    Exercise 3.py

--- Chapter 5- Dictionaries/Exercises/Exercise 3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise 3.py
@@ -4,3 +4,18 @@ calls with a loop that runs through the dictionary’s keys and values. When you
 
 to your glossary.When you run your program again, these new words and meanings should automatically be included in the output.
 '''
+
+#same glossary dictionary modified by adding five more keys and values
+glossary = {'if': 'This condition can be any expression that evaluates to either True or False',
+            'elif': 'it is used to include multiple conditional expressions after the if condition or even in between if and else conditions. ',
+            'else': 'it is used in conditional if statements, and decides what to do if the condition is False.',
+            'print': 'it is used to display a ceratin messge or call out a variable',
+            'input':'it helps the user to input the values',
+            'dictionary': 'A collection of key-value pairs.',
+            'key': 'The first item in a dictionary.',
+            'value': 'An item associated with a key in a dictionary.',
+            'float': 'A numerical value with a decimal point.',
+            'boolean values': 'It consists of True or False.'}
+
+for word, definition in glossary.items():
+    print(f"\n{word.title()}: {definition}")
